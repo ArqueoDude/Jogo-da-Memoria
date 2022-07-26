23ec082c7facd7189f70e1d7c2a42403b2e08ccf
Filipe Simoes <109987257+ArqueoDude@users.noreply.github.com>
2022-07-26 20:06:12 -0300

    Finalizado
    Jogo completo

---
+++ assets/scripts/game.js
@@ -0,0 +1,109 @@
+let game = {
+
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    dogs: ['beagle',
+    'boxer',
+    'buldogue',
+    'bull-terrier',
+    'chow-chow',
+    'corgi',
+    'doberman',
+    'fox-paulistinha',
+    'golden-retriever',
+    'poodle'],
+
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id) [0];
+
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        }else{
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function() {
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
+
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards(){
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver() {
+
+        return this.cards.filter(card => !card.flipped).length == 0; 
+    },
+    
+    cards : null,
+
+    createCardsFromDogs: function() {
+
+        this.cards = [];
+    
+        this.dogs.forEach((dog) => {
+            this.cards.push(this.createPairFromDog(dog));
+        })
+    
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
+        return this.cards;
+    },
+    
+    createPairFromDog: function(dog) {
+    
+        return[{
+            id: this.createIdWithDog(dog),
+            icon: dog,
+            flipped: false,        
+        }, {
+            id: this.createIdWithDog(dog),
+            icon: dog,
+            flipped: false,
+        }]
+    },
+    
+    createIdWithDog: function(dog) {
+        return dog + parseInt(Math.random() *1000);
+    },
+
+    shuffleCards: function(cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+    
+        while (currentIndex !== 0) {
+    
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex--;
+    
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
+        }
+    },
+
+}
